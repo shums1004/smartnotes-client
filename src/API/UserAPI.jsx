d90634596dcdc54c06c8689293e2f8208bc27061
shums1004 <shums1004@gmail.com>
2025-05-02 22:31:27 -0700

    Changed the base url

--- src/API/UserAPI.jsx
+++ src/API/UserAPI.jsx
@@ -7,7 +7,6 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 export const signin = async (email, password) => {
-
  const res =  await fetch(`${BASE_URL}/api/login`, {
     method: 'POST',
     headers: {
